0d73a613b558d76a4a0e3b18e7d3217f10731eeb
Mikko Voima <mikko.voima@cs.tamk.fi>
2018-02-21 11:46:10 +0200

    Flocking implemented

---
+++ Assets/Code/AgentBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentBehaviour : MonoBehaviour
+{
+    public AgentController _controller;
+    public int neighboursFound = 0;
+
+    public void AddController(AgentController controller)
+    {
+        _controller = controller;
+    }
+
+    Vector3 GetSeparationVector(Transform target)
+    {
+        Vector3 diff = transform.position - target.transform.position;
+        float diffLen = diff.magnitude;
+        float scaler = Mathf.Clamp01(1.0f - diffLen / _controller._agentDistance);
+        return diff * (scaler / diffLen);
+    }
+
+    void Update()
+    {
+        Vector3 currentPos = transform.position;
+        Quaternion currentRot = transform.rotation;
+
+        float velocity = _controller.Velocity;
+
+        Vector3 separation = Vector3.zero;
+        Vector3 alignment = transform.forward;
+        Vector3 cohesion = _controller.transform.position;
+
+        Collider[] neighbours = Physics.OverlapSphere(currentPos, _controller._agentDistance, _controller._agentMask);
+        neighboursFound = neighbours.Length - 1;
+
+        foreach (Collider agent in neighbours)
+        {
+            if (agent.gameObject == this.gameObject)
+                continue;
+
+            separation += GetSeparationVector(agent.transform);
+            alignment += agent.transform.forward;
+            cohesion += agent.transform.position;
+        }
+
+        float average = 1.0f / (1.0f * neighbours.Length);
+
+        alignment *= average;
+        cohesion *= average;
+        cohesion = (cohesion - currentPos).normalized;
+
+        Vector3 newDirection = separation + alignment + cohesion;
+        Quaternion newRotation = Quaternion.FromToRotation(Vector3.forward, newDirection.normalized);
+
+        if (currentRot != newRotation)
+        {
+            transform.rotation = Quaternion.Slerp(currentRot, newRotation, Mathf.Exp(-4.0f * Time.deltaTime));
+        }
+
+        // Finally move
+        transform.position += transform.forward * (_controller.Velocity * Time.deltaTime);
+
+        Debug.DrawLine(currentPos, (currentPos + transform.forward * 3f), Color.red);
+    }
+}
---
+++ Assets/Code/AgentController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class AgentController : MonoBehaviour
+{
+    public GameObject _prefab;
+    public int _prefabsToSpawn = 20;
+    public float _spawnRadius = 20f;
+    public float _agentVelocity = 5f;
+    public float _agentDistance = 3f;
+    public float _randomRotation = 5f;
+    public LayerMask _agentMask;
+
+    public float Velocity { get { return _agentVelocity; } }
+
+    private void Start()
+    {
+        if (_prefab == null)
+        {
+            Debug.LogError("Define prefab in " + gameObject);
+            return;
+        }
+
+        for (int i = 0; i < _prefabsToSpawn; i++)
+        {
+            SpawnPrefab(i + 1);
+        }
+    }
+
+    private void Update()
+    {
+        transform.position += transform.forward * Velocity * Time.deltaTime;
+    }
+
+    private void SpawnPrefab(int number)
+    {
+        float x = Random.Range(-_randomRotation, _randomRotation);
+        float y = Random.Range(-_randomRotation, _randomRotation);
+        float z = Random.Range(-_randomRotation, _randomRotation);
+
+        Quaternion rotation = Quaternion.Euler(x, y, z);
+        Vector3 position = UnityEngine.Random.insideUnitSphere * _spawnRadius;
+
+        GameObject go = Instantiate(_prefab, position, rotation, transform);
+        go.name = "Agent " + number;
+
+        AgentBehaviour behaviour = go.GetComponent<AgentBehaviour>();
+
+        behaviour.AddController(this);
+    }
+}
